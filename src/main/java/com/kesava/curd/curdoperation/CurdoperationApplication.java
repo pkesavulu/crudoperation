f6bd6afc3aa52f9d0d809fd0660b07ab56d529e2
pkesavulu <pkesavulu.91@gmail.com>
2019-08-24 15:13:30 +0530

    curdoperation project

---
+++ src/main/java/com/kesava/curd/curdoperation/CurdoperationApplication.java
@@ -0,0 +1,13 @@
+package com.kesava.curd.curdoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdoperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdoperationApplication.class, args);
+	}
+
+}
